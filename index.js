d0e919b0d440d2303c4f735e834f34e1949a164b
Jason Farnsworth <jdhiro@users.noreply.github.com>
2019-01-19 15:03:55 -0800

    fix port issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const PORT = process.env.port || 8080
+const PORT = process.env.port || 8443
 
 app.use(express.static(path.join(__dirname, 'build')))
 
